2dd16bd884db7913f2501eca1cb50af4214bd3de
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-06-22 01:46:25 +0800

    fixed design issue

--- Screens/StartGameScreen.js
+++ Screens/StartGameScreen.js
@@ -5,7 +5,7 @@ const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
       <TextInput
-        styles={styles.numberInput}
+        style={styles.numberInput}
         maxLength={2}
         keyboardType="number-pad"
         autoCapitalize="none"
@@ -21,21 +21,21 @@ const styles = StyleSheet.create({
   inputContainer: {
     marginTop: 100,
     marginHorizontal: 24,
-    borderRadius: 8,
     elevation: 4,
     padding: 16,
-    backgroundColor: "#72063",
+    backgroundColor: "#72063c",
+    borderRadius: 8,
     shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
-    shadowOpacity: 1,
+    shadowOpacity: 0.25,
   },
   numberInput: {
     height: 50,
     width: 50,
     fontSize: 32,
     borderBottomColor: "#ddb52f",
-    borderWidth: 2,
+    borderBottomWidth: 2,
     color: "#ddb52f",
     marginVertical: 8,
     fontWeight: "bold",
--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 
 const PrimaryButton = ({ children }) => {
+  function pressHandlder() {
+    console.log("Pressed!");
+  }
   return (
-    <View>
-      <Text>{children}</Text>
-    </View>
+    <Pressable onPress={pressHandlder}>
+      <View>
+        <Text>{children}</Text>
+      </View>
+    </Pressable>
   );
 };
 export default PrimaryButton;
