cbc26c6cde8a80f05a4ef65d78e7e6b77c5b5a23
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-06-23 22:29:55 +0800

    Adding a background image and learn what is the difference of style and imageStyle

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require("./assets/images/background (1).png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -15,5 +22,8 @@ const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
     backgroundColor: "#ddb52f",
+  },
+  backgroundImage: {
+    opacity: 0.75,
   },
 });
