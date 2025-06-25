e76db9e68604a91724f247ef368638e406c981c4
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-06-26 04:32:26 +0800

    creating title component to reused it later

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 import GameScreen from "./screens/GameScreen";
--- Screens/GameScreen.js
+++ Screens/GameScreen.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text, TextInput, StyleSheet } from "react-native";
+import Title from "../components/Title";
 
 const GameScreen = () => {
   return (
     <View style={styles.screen}>
-      <Text>Opponent's Guess</Text>
+      <Title>Opponent's Guess</Title>
+
       {/* Guess */}
       <View>
         <Text>Higher or Lower?</Text>
@@ -18,6 +20,15 @@ const GameScreen = () => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+    padding: 12,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#ddb52f",
+    textAlign: "center",
+    borderWidth: 2,
+    borderColor: "#ddb52f",
     padding: 12,
   },
 });
