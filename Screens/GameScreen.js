51c56f0ec843fba380642c17c7d245cc93110f81
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-06-27 19:35:35 +0800

    generate random numberfunction

--- Screens/GameScreen.js
+++ Screens/GameScreen.js
@@ -2,6 +2,16 @@ import React from "react";
 import { View, Text, TextInput, StyleSheet } from "react-native";
 import Title from "../components/Title";
 
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
 const GameScreen = () => {
   return (
     <View style={styles.screen}>
